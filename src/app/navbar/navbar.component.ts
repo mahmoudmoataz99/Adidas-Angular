e8dc37794e7171f7ea3f0853daf85cc3ae531f30
mahmoudmoataz99 <mahmoudmoataz99@gmail.com>
2024-11-14 14:09:55 +0200

    2:09

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import data from '../products.json';
-
 import { NgFor, NgClass, NgIf } from '@angular/common';
 import { RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -18,34 +17,29 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
-  products: Array<any>;
-  allGenders: Array<any>;
-  genders: Array<string>;
+
+export class NavbarComponent implements OnInit {
+  products?: Array<any>;
+  allGenders?: Array<any>;
+  genders?: Array<string>;
   genderSearch: string | null = null;
   searchForm: any;
-  cartItems:number = 5;
+  cartItems?:any;
 
-  constructor(private router: Router, private formBuilder: FormBuilder) {
+  constructor(private router: Router, private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
     this.products = data.items;
-
+    this.cartItems = this.products.length;
     this.allGenders = this.products.filter(
       (item) => item.gender !== 'Unisex'
     );
     this.genders = [...new Set(this.allGenders.map((item) => item.gender))];
 
-    this.searchForm = this.formBuilder.group({
-      searchItem: ['', Validators.required],
-    });
+    this.searchForm = this.formBuilder.group({searchItem: ['', Validators.required],});
   }
 
-  navigate() {
-    if (this.genderSearch) {
-      this.router.navigate(['/category', this.genderSearch]);
-    }
-  }
+  navigate = () => this.genderSearch && this.router.navigate(['/category', this.genderSearch]);
 
-  searchFunc() {
-    this.searchForm.valid ? (this.router.navigate(['/search',this.searchForm.value.searchItem]),this.searchForm.reset()) : alert('Type anything to search for');
-  }
+  searchFunc = () => this.searchForm.valid ? (this.router.navigate(['/search',this.searchForm.value.searchItem]),this.searchForm.reset()) : alert('Type anything to search for');
 }
